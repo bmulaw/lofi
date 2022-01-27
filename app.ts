0d1c3798bd58f3eeb02986b3ed2ed20251cfaa69
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-53.bu.edu>
2022-01-26 21:59:01 -0500

    added slider and converted to ts

---
+++ app.ts
@@ -0,0 +1,30 @@
+let speed: number = 1.0
+
+const changeSlide = (): void => {
+    let slider: any = document.getElementById("myRange");
+    let output: any = document.getElementById("demo");
+    output.innerHTML = slider.value;
+    speed = slider.value / 75
+}
+
+const ctx: AudioContext = new AudioContext();
+let audio: any;
+
+
+const getSong = (song) => {
+    fetch('./music/' + song + '.mp3')
+    .then(data => data.arrayBuffer())
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+        audio = decodedAudio;
+    });
+    playback(audio);
+}
+
+const playback = (audio) => {
+    const playSound: AudioBufferSourceNode = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.playbackRate.value = speed;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
+}
