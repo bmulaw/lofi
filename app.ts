6c73c83a6a63dcb47ac744ebedf03d42e692c6e8
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-53.bu.edu>
2022-02-13 00:40:05 -0500

    Angular framework

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const ctx: AudioContext = new AudioContext();
 let audio: any;
 
 
-const getSong = (song) => {
+const getSong = (song: string) => {
     fetch('./music/' + song + '.mp3')
     .then(data => data.arrayBuffer())
     .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
@@ -21,7 +21,7 @@ const getSong = (song) => {
     playback(audio);
 }
 
-const playback = (audio) => {
+const playback = (audio: any) => {
     const playSound: AudioBufferSourceNode = ctx.createBufferSource();
     playSound.buffer = audio;
     playSound.playbackRate.value = speed;
