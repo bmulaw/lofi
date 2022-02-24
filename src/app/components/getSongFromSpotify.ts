197172b0bd9edfc7e989f3f1a10da578ebe56dac
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-53.bu.edu>
2022-02-24 17:21:27 -0500

    initial set for playing spotify music on app

---
+++ src/app/components/getSongFromSpotify.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'spotify-song',
+    templateUrl: './getSongFromSpotify.component.html'
+})
+
+export class GetSongFromSpotify implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
+
